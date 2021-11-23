c7c8929b47fa9cb467c5d9e618f64da8d35629d3
Baptiste Nicolet <baptiste.nicolet@epfl.ch>
2021-11-23 09:29:48 +0100

    Ported Voronoi mass matrix computation

--- python/geometry.py
+++ python/geometry.py
@@ -33,6 +33,61 @@ def average_edge_length(verts, faces):
 
     return (A + B + C).sum() / faces.shape[0] / 3
 
+def massmatrix_voronoi(verts, faces):
+    """
+    Compute the area of the Voronoi cell around each vertex in the mesh.
+    http://www.alecjacobson.com/weblog/?p=863
+
+    params
+    ------
+
+    v: vertex positions
+    f: triangle indices
+    """
+    # Compute edge lengths
+    l0 = (verts[faces[:,1]] - verts[faces[:,2]]).norm(dim=1)
+    l1 = (verts[faces[:,2]] - verts[faces[:,0]]).norm(dim=1)
+    l2 = (verts[faces[:,0]] - verts[faces[:,1]]).norm(dim=1)
+    l = torch.stack((l0, l1, l2), dim=1)
+
+    # Compute cosines of the corners of the triangles
+    cos0 = (l[:,1].square() + l[:,2].square() - l[:,0].square())/(2*l[:,1]*l[:,2])
+    cos1 = (l[:,2].square() + l[:,0].square() - l[:,1].square())/(2*l[:,2]*l[:,0])
+    cos2 = (l[:,0].square() + l[:,1].square() - l[:,2].square())/(2*l[:,0]*l[:,1])
+    cosines = torch.stack((cos0, cos1, cos2), dim=1)
+
+    # Convert to barycentric coordinates
+    barycentric = cosines * l
+    barycentric = barycentric / torch.sum(barycentric, dim=1)[..., None]
+
+    # Compute areas of the faces using Heron's formula
+    areas = 0.25 * ((l0+l1+l2)*(l0+l1-l2)*(l0-l1+l2)*(-l0+l1+l2)).sqrt()
+
+    # Compute the areas of the sub triangles
+    tri_areas = areas[..., None] * barycentric
+
+    # Compute the area of the quad
+    cell0 = 0.5 * (tri_areas[:,1] + tri_areas[:, 2])
+    cell1 = 0.5 * (tri_areas[:,2] + tri_areas[:, 0])
+    cell2 = 0.5 * (tri_areas[:,0] + tri_areas[:, 1])
+    cells = torch.stack((cell0, cell1, cell2), dim=1)
+
+    # Different formulation for obtuse triangles
+    # See http://www.alecjacobson.com/weblog/?p=874
+    cells[:,0] = torch.where(cosines[:,0]<0, 0.5*areas, cells[:,0])
+    cells[:,1] = torch.where(cosines[:,0]<0, 0.25*areas, cells[:,1])
+    cells[:,2] = torch.where(cosines[:,0]<0, 0.25*areas, cells[:,2])
+
+    cells[:,0] = torch.where(cosines[:,1]<0, 0.25*areas, cells[:,0])
+    cells[:,1] = torch.where(cosines[:,1]<0, 0.5*areas, cells[:,1])
+    cells[:,2] = torch.where(cosines[:,1]<0, 0.25*areas, cells[:,2])
+
+    cells[:,0] = torch.where(cosines[:,2]<0, 0.25*areas, cells[:,0])
+    cells[:,1] = torch.where(cosines[:,2]<0, 0.25*areas, cells[:,1])
+    cells[:,2] = torch.where(cosines[:,2]<0, 0.5*areas, cells[:,2])
+
+    # Sum the quad areas to get the voronoi cell
+    return torch.zeros_like(verts).scatter_add_(0, faces, cells).sum(dim=1)
 
 def laplacian_cot(verts, faces):
     """
