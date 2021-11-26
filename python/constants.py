a90e89e27d5559d395bb2aa0691937549b0634ef
Baptiste Nicolet <baptiste.nicolet@epfl.ch>
2021-11-26 14:23:18 +0100

    Updated constant paths

--- python/constants.py
+++ python/constants.py
@@ -1,7 +1,9 @@
 import os
 
-OUTPUT_DIR = os.path.realpath("/home/bnicolet/Documents/OT/output_new/")
-SCENES_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), "../scenes"))
-REMESH_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "botsch-kobbelt-remesher-libigl/build")
-BLEND_SCENE = os.path.realpath("/home/bnicolet/Documents/OT/diff_geom/render2.blend")
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+#OUTPUT_DIR = os.path.realpath(os.path.join(ROOT_DIR, "output"))
+OUTPUT_DIR = "/home/bnicolet/Documents/OT/output_new"
+SCENES_DIR = os.path.realpath(os.path.join(ROOT_DIR, "scenes"))
+REMESH_DIR = os.path.join(ROOT_DIR, "botsch-kobbelt-remesher-libigl/build")
+BLEND_SCENE = os.path.join(SCENES_DIR, "render.blend")
 BLENDER_EXEC = "blender2.8"
