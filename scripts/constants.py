4f1a5be414f4aa9a68e0595c4748a3d91d227ed8
Baptiste Nicolet <baptiste.nicolet@epfl.ch>
2021-12-02 08:46:13 +0100

    Finalized paths file

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,9 +1,8 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
-#OUTPUT_DIR = os.path.realpath(os.path.join(ROOT_DIR, "output"))
-OUTPUT_DIR = "/home/bnicolet/Documents/OT/output_new"
+OUTPUT_DIR = os.path.realpath(os.path.join(ROOT_DIR, "output")) # Change this if you want to output the data and figures elsewhere
 SCENES_DIR = os.path.realpath(os.path.join(ROOT_DIR, "scenes"))
 REMESH_DIR = os.path.join(ROOT_DIR, "botsch-kobbelt-remesher-libigl/build")
 BLEND_SCENE = os.path.join(SCENES_DIR, "render.blend")
-BLENDER_EXEC = "blender2.8"
+BLENDER_EXEC = "blender" # Change this if you have a different blender installation
